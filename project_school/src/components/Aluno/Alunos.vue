dd8d72c1a9cab667e2824d55f4d895a636582e1e
Priscila marques <priscila.marques@animaeducacao.com.br>
2020-11-25 07:56:35 -0300

    first commit

---
+++ project_school/src/components/Aluno/Alunos.vue
@@ -0,0 +1,104 @@
+<template>
+  <div >
+    <titulo texto="Aluno"/>
+    <div>
+    <input type="text" placeholder="Nome do Aluno" v-model="nome" v-on:keyup.enter="addAluno()">
+    <button class="btn btnInput" @click="addAluno()">Adicionar</button>
+    </div>
+
+    <table>
+      <thead>
+        <th>Mat.</th>
+        <th>Nome</th>
+        <th>Opções</th>
+      </thead>
+      <tbody v-if="alunos.length">
+        <tr v-for="(aluno, index) in alunos" :key="index">
+          <td>{{aluno.id}}</td>
+          <td>{{aluno.nome}} {{aluno.sobrenone}}</td>
+          <td>
+            <button class="btn btn_Danger" @click="remover(aluno)">Remover</button>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot v-else>
+        Nenhum Aluno Encontrado
+      </tfoot>
+    </table>
+
+  </div>
+</template>
+
+<script>
+import Titulo from '../_share/Titulo'
+
+export default {
+  components: {
+    Titulo
+  },
+  data(){
+    return {
+      titulo: 'Aluno',
+      nome: '',
+      alunos: [
+
+      ]
+    }
+  },
+  created() {
+    this.$http
+    .get('http://localhost:3000/alunos')
+    .then(res => res.json())
+    .then(alunos => this.alunos = alunos)
+  },
+  props: {
+  },
+  methods: {
+    addAluno() {
+      let _aluno = {
+        nome: this.nome,
+        sobrenome: ""
+      }
+
+      this.$http
+        .post('http://localhost:3000/alunos', _aluno)
+        .then(res => res.json())
+        .then(alunoRetornado => {
+          this.alunos.push(alunoRetornado);
+          this.nome = '';
+        })
+    },
+    remover(aluno) {
+        this.$http
+          .delete(`http://localhost:3000/alunos/${aluno.id}`)
+          .then(() => {
+            let indice = this.alunos.indexOf(aluno);
+            this.alunos.splice(indice, 1);
+          })
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input{
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  color: #687f7f;
+  display: inline;
+}
+.btnInput{
+  border: 0px;
+  padding: 20px;
+  font-size: 1.3em;
+  display: inline;
+  background-color: rgb(116, 115, 115);
+}
+.btnInput:hover{
+  padding: 20px;
+  margin: 0px;
+  border: 0px;
+}
+</style>
